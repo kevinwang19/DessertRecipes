da693f3d87e0b167fc0590cfeb036faaabca5d1a
k393wang <k393wang@uwaterloo.ca>
2023-05-27 16:55:34 -0400

    Initial Commit

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Kevin Wang on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
